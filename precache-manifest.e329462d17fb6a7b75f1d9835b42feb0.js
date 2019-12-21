88b2904c2d22f72a1ffcb17ce1ddda29eaacbfeb
Tim Paine <t.paine154@gmail.com>
2019-12-21 01:35:35 -0500

    Updates

---
+++ precache-manifest.e329462d17fb6a7b75f1d9835b42feb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffecd6affd28cea66575b08ee47ceb0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d258ba587e10821e16d",
+    "url": "/static/css/main.81ec346e.chunk.css"
+  },
+  {
+    "revision": "686332b906a25c76c636",
+    "url": "/static/js/2.d1bf42a1.chunk.js"
+  },
+  {
+    "revision": "5d258ba587e10821e16d",
+    "url": "/static/js/main.264d8256.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
